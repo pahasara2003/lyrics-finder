738cb721b88e5ede674d6f726269037cc29115a0
pahasara wickramasinghe <138836186+pahasara2003@users.noreply.github.com>
2023-12-13 22:24:54 +0530

    edit head data

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png"></link>
+        <meta name="theme-color" content="#fff" />
+      </head>
       <body className="min-h-screen flex flex-col justify-between ">
         <Provider> {children} </Provider>
         <footer className="p-4  w-[100%]  text-center text-sm bg-gray-100">
